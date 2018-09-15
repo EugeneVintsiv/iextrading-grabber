3df1e9aeeb86412f296b2cd17ae4599013999f97
Eugene Vintsiv <eugene.vintsiv@gmail.com>
2018-09-16 00:25:14 +0300

    init commit

---
+++ src/main/java/io/github/eugenevintsiv/iextradinggrabber/IextradingGrabberApplication.java
@@ -0,0 +1,12 @@
+package io.github.eugenevintsiv.iextradinggrabber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IextradingGrabberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IextradingGrabberApplication.class, args);
+	}
+}
